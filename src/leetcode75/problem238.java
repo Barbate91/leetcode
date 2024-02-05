69910511b3571a2cbeeca59ed6c0faae32a58a65
jake.armstrong <jake.armstrong@servicenow.com>
2024-02-05 14:37:50 -0800

    problem 238 shell

---
+++ src/leetcode75/problem238.java
@@ -0,0 +1,13 @@
+public class problem238 {
+	public int[] productExceptSelf(int[] nums) {
+		// Calculate product to left of each element
+		// Calc to right of each element
+		// Then calc prod of left and right
+		// O(3n) -> O(n)
+		int[] productArr = new int[nums.length];
+
+		for (int i = 0; i < nums.length; i++) {
+
+		}
+	}
+}
